0cc8b77b7d9cfdeb606a25b97743c7de9aff812b
Shivanshu Sharma <shivanshu1177@chitkara.edu.in>
2022-03-30 11:15:34 +0530

    simple sol.

---
+++ New folder/Maths/factOfNum1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n){
+    int res = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        /* code */
+        res *= i ;
+    }
+    return res;
+    
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << fact(n);
+    return 0;
+}
