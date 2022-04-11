a37b2ee3db0434dca8587b9f71c8cf9ebf5374c6
Shivanshu Sharma <shivanshu1177@chitkara.edu.in>
2022-04-11 12:44:36 +0530

    lcm using algo

---
+++ New folder/Maths/lcmEucledialsAlgo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    else
+    {
+        return gcd(b, a % b);
+    }
+}
+
+int lcm(int a, int b){
+    return (a*b)/gcd(a, b);
+}
+
+int main()
+{
+    /* code */
+    cout << lcm(4, 6);
+}
