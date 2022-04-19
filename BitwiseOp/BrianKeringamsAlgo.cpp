85f805735942ca50b7ea8fb95a5c736f88c68933
Shivanshu Sharma <shivanshu1177@chitkara.edu.in>
2022-04-19 23:39:18 +0530

    algo

---
+++ BitwiseOp/BrianKeringamsAlgo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int countSet(int n){
+    int res = 0;
+    while (n>0)
+    {
+        n = (n & (n-1));
+        res++ ;
+
+    }
+    return res;
+    
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << countSet(n);
+}
