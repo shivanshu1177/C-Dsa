70911febc5223be83520df51795f68670d625498
Shivanshu Sharma <shivanshu1177@chitkara.edu.in>
2022-05-17 22:55:05 +0530

    rev array

---
+++ Arrays/reverseAnArray.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int revs(int arr[], int n){
+    int s = 0;
+    int e = n-1;
+    while(s<=e){
+        swap(arr[s], arr[e]);
+        s++;
+        e--;
+    }
+    return 0;
+
+}
+int give(int arr[], int n){
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
+int main()
+{
+    /* code */
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    revs(arr, n); 
+    give(arr, n);
+    return 0;
+}
