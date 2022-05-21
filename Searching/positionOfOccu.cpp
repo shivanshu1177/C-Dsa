44f514ca8aab8921097fd92b7b5d1f1905c48bec
Shivanshu Sharma <shivanshu1177@chitkara.edu.in>
2022-05-21 18:20:37 +0530

    ,

---
+++ Searching/positionOfOccu.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+int firstPos(int arr[], int n, int key)
+{
+    int s = 0, e = n - 1;
+    int ans = -1;
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        else if (key > arr[mid])
+        {
+            s = mid + 1;
+        }
+        else if (key < arr[mid])
+        {
+            e = mid - 1;
+        }
+
+        mid = s + (e - s)/2;
+    }
+    return ans;
+}
+int lastPos(int arr[], int n, int key)
+{
+    int s = 0, e = n - 1;
+    int ans = -1;
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else if (key > arr[mid])
+        {
+            s = mid + 1;
+        }
+        else if (key < arr[mid])
+        {
+            e = mid - 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int key;
+    cin >> key;
+    cout << "1st at " << firstPos(arr, n, key)<< endl;
+    cout << "last at " << lastPos(arr, n, key)<< endl;;
+}
